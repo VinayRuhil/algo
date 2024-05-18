c3bfbf8740052c669347c86fd67dd44b2fab116a
VinayRuhil <114719635+VinayRuhil@users.noreply.github.com>
2024-05-18 07:44:49 +0530

    Create Strassen multiplication.cpp

---
+++ Strassen multiplication.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to multiply two matrices using Strassen algorithm
+void strassen(int A[][2], int B[][2], int C[][2]) {
+    int m1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    int m2 = (A[1][0] + A[1][1]) * B[0][0];
+    int m3 = A[0][0] * (B[0][1] - B[1][1]);
+    int m4 = A[1][1] * (B[1][0] - B[0][0]);
+    int m5 = (A[0][0] + A[0][1]) * B[1][1];
+    int m6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    int m7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
+
+    C[0][0] = m1 + m4 - m5 + m7;
+    C[0][1] = m3 + m5;
+    C[1][0] = m2 + m4;
+    C[1][1] = m1 - m2 + m3 + m6;
+}
+
+// Function to display a matrix
+void displayMatrix(int matrix[][2], int row, int col) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int A[2][2] = {{1, 2}, {3, 4}};
+    int B[2][2] = {{5, 6}, {7, 8}};
+    int C[2][2] = {0};
+
+    strassen(A, B, C);
+
+    cout << "Resultant Matrix:" << endl;
+    displayMatrix(C, 2, 2);
+
+    return 0;
+}
